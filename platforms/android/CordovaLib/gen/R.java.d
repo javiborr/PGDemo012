8ae59de8ed2e87a45289626ca94bda3900b33c88
javiborr <javiborr@gmail.com>
2014-01-07 18:45:29 +0100

    Template Cordova app

---
+++ platforms/android/CordovaLib/gen/R.java.d
@@ -0,0 +1,1 @@
+ : D:\zpj\Azure\PhoneGap\pgdemo012\platforms\android\CordovaLib\bin\AndroidManifest.xml \
---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+D:\zpj\Azure\PhoneGap\pgdemo012\platforms\android\gen\com\bt\pgdemo012\R.java \
+ : D:\zpj\Azure\PhoneGap\pgdemo012\platforms\android\res\drawable\icon.png \
+D:\zpj\Azure\PhoneGap\pgdemo012\platforms\android\res\drawable-hdpi\icon.png \
+D:\zpj\Azure\PhoneGap\pgdemo012\platforms\android\res\drawable-ldpi\icon.png \
+D:\zpj\Azure\PhoneGap\pgdemo012\platforms\android\res\drawable-mdpi\icon.png \
+D:\zpj\Azure\PhoneGap\pgdemo012\platforms\android\res\drawable-xhdpi\icon.png \
+D:\zpj\Azure\PhoneGap\pgdemo012\platforms\android\res\values\strings.xml \
+D:\zpj\Azure\PhoneGap\pgdemo012\platforms\android\res\xml\config.xml \
+D:\zpj\Azure\PhoneGap\pgdemo012\platforms\android\bin\AndroidManifest.xml \
